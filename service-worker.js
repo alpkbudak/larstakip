b224cb552de7271b367c29296c68a3800793cb4f
alpkbudak <yunuskbudak@gmail.com>
2026-03-22 15:33:46 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,1 @@
-const ASSETS = [
-  '/larstakip/',
-  '/larstakip/index.html',
-  '/larstakip/manifest.json'
-];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
